44426f30183e8ce8df3240d44ed3007f5ee27924
Phil Youn <phillipyoun@gmail.com>
2020-10-29 23:04:05 -0700

    Generate profile and show page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,23 @@
 class ProfilesController < ApplicationController
+  def show
+    @profile = Profile.find(params[:id])
+  end
+  
   def new
   end
 
   def create
-    render plain: params[:profile].inspect
+    # render plain: params[:profile].inspect
+
+    @profile = Profile.new(params.require(:profile).permit(:first_name, :last_name, :title, :logline, :phone))
+
+    @profile.save
+    redirect_to @profile
   end
 end
+
+
+private
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :title, :logline, :phone)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -5,3 +5,4 @@ Rails.application.routes.draw do
 
   root 'landing_page#index'
 end
+
